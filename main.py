b9b70b2c7443da012e471bf385e300ae968719fb
BuildTools <unconfigured@null.spigotmc.org>
2022-11-27 12:10:35 +0100

    PyCharm

--- main.py
+++ main.py
@@ -1,40 +1,7 @@
 import pygame
-
-class PlanetExplorer:
-    def __init__(self):
-        self._running = True
-        self._display = None
-        self.size = self.weight, self.height = 1080, 720
-
-    def on_init(self):
-        pygame.init()
-        pygame.display.set_caption("Planet Explorer")
-        self._display = pygame.display.set_mode(self.size, pygame.HWSURFACE | pygame.DOUBLEBUF)
-        self._running = True
-
-    def on_event(self, event):
-        if event.type == pygame.QUIT:
-            self._running = False
-
-    def on_loop(self):
-        pass
-
-    def on_render(self):
-        pass
-
-    def on_cleanup(self):
-        pygame.quit()
-
-    def on_execute(self):
-        self.on_init()
-        while self._running:
-            for event in pygame.event.get():
-                self.on_event(event)
-            self.on_loop()
-            self.on_render()
-        self.on_cleanup()
-
+from pytmx.util_pygame import load_pygame
+from game import Game
 
 if __name__ == "__main__":
-    theApp = PlanetExplorer()
-    theApp.on_execute()
+    game = Game()
+    game.on_execute()
