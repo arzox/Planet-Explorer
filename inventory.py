efadede14313373924e07f5728c3b28f875043b9
CelianJok <67053020+CelianJok@users.noreply.github.com>
2022-11-30 16:53:45 +0100

    Barre d'inventaire partielle

---
+++ inventory.py
@@ -0,0 +1,59 @@
+import pygame
+
+
+class Slot(pygame.sprite.Sprite):
+
+    def __init__(self, id_slot):
+        super().__init__()
+
+        self.id_slot = id_slot
+        self.counter = None
+        self.item_in_slot = None
+        self.is_empty = True
+        self.image = pygame.image.load('assets/inventory/default.jpg')
+        self.rect = self.image.get_rect()
+
+    def add_item(self, item):
+        if self.is_empty is True:
+            self.item_in_slot = item
+            self.is_empty = False
+            self.counter = 1
+        elif self.is_empty is False and item.type == self.item_in_slot:
+            self.counter += 1
+
+    def set_position(self, position: (int, int)):
+        self.rect.x, self.rect.y = position
+
+
+class Inventory:
+
+    def __init__(self):
+
+        self.number = 9
+        self.slots = []
+
+        id_slot = 1
+        slots_x = 100
+        slots_y = 630
+        for i in range(self.number):
+            slot = Slot(id_slot)
+            self.slots.append(slot)
+            slot.set_position((slots_x, slots_y))
+            id_slot += 1
+            slots_x += 100
+
+    def display(self, screen):
+        for slot in self.slots:
+            screen.blit(slot.image, slot.rect)
+
+    def try_to_add_item_in_slot(self, item):
+        loop = True
+        id_slot = 1
+        slot = Slot(id_slot)
+        while loop is not False:
+            if slot.is_empty is True:
+                slot.add_item(item)
+                id_slot = 1
+                loop = False
+            else:
+                id_slot += 1
