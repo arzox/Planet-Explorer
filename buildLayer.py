8e79e253ca1c6fa8b0e879f43b7fb62462b84f68
Aimery <aimery.pellerin@gmail.com>
2022-11-30 18:16:45 +0100

    Build

---
+++ buildLayer.py
@@ -0,0 +1,64 @@
+import pygame
+import pytmx
+from build import Build
+
+
+class HoloTile(pygame.sprite.Sprite):
+    def __init__(self, pos, surf):
+        super().__init__()
+        self.image = surf
+        self.rect = self.image.get_rect(topleft=pos)
+
+
+class BuildLayer:
+    def __init__(self, tmx_data: pytmx.TiledMap, all_sprites):
+        self.tmx_data = tmx_data
+        self.all_sprites = all_sprites
+        self.tile_size = self.tmx_data.tileheight
+        # sprite groups
+        self.plan_sprites = pygame.sprite.Group()
+
+        # graphics
+        # self.plan_surf = pygame.image.load(plan.value)
+
+        self.create_ground_grid()
+        self.create_ground_rects()
+
+    def create_ground_grid(self):
+        map_height, map_width = self.tmx_data.height, self.tmx_data.width
+
+        self.grid = [[[] for col in range(map_width)] for row in range(map_height)]
+        for x, y, _ in self.tmx_data.get_layer_by_name("ground").tiles():
+            self.grid[y][x].append('B')
+
+    def create_ground_rects(self):
+        self.ground_rects = []
+        for index_row, row in enumerate(self.grid):
+            for index_col, cell in enumerate(row):
+                if 'B' in cell:
+                    x = index_col * self.tile_size
+                    y = index_row * self.tile_size
+                    rect = pygame.Rect(x, y, self.tile_size, self.tile_size)
+                    self.ground_rects.append(rect)
+
+    def get_build(self, point):
+        for rect in self.ground_rects:
+            if rect.collidepoint(point):
+                x = rect.x // self.tile_size
+                y = rect.y // self.tile_size
+
+                if 'B' in self.grid[y][x]:
+                    self.grid[y][x].append('X')
+                    self.create_build()
+
+    def create_build(self):
+        self.plan_sprites.empty()
+        for index_row, row in enumerate(self.grid):
+            for index_col, cell in enumerate(row):
+                if 'X' in cell:
+                    print(self.all_sprites)
+                    sprite = HoloTile(pos=(index_col * self.tile_size, index_row * self.tile_size),
+                             surf=pygame.image.load(Build.HOLO.value),
+                             #groups=[self.all_sprites, self.plan_sprites]
+                             )
+                    self.all_sprites.add(sprite)
--- idea/Build.md
+++ idea/Build.md
@@ -4,3 +4,12 @@
 * Prévisualisation de la construction en face du joueur
 * rotation du bloc
 * rajout de bloc facil
+
+## Objet
+#### Liste
+* Fournisseur d'oxygène
+* Forge
+* S.A.S.
+* Couloir
+* Stockage
+* Workbench
