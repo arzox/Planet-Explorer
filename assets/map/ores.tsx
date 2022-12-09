8eeb2869867eee112028f26fafc7baedcad6a921
aimery <aimery.pellerin@gmail.com>
2022-12-09 08:52:01 +0100

    FixBug

--- assets/map/ores.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="ores" tilewidth="32" tileheight="32" tilecount="56" columns="7">
- <image source="../../../../Downloads/ores.png" width="224" height="256"/>
-</tileset>
